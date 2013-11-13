668e7218104276d8697634198832e74caca35abe
unknown <pi2684@SKCBC2061.emea.att.com>
2013-11-13 14:07:44 +0100

    Reworked to be more user friendly, prints help, feature freeze for version 1.0

--- systeminfo.py
+++ systeminfo.py
@@ -3,6 +3,7 @@
 import sys
 import getopt
 import re
+import os
 from string import Template
 from proc.cpu import Cpu
 from proc.memory import Memory
@@ -16,30 +17,90 @@ asset_types = ['cpu', 'memory', 'pci', 'fcms', 'disk', 'system']
 
 def main():
         cmds = []
+        action = ''
+        asset_param = ''
         try:
             opts, args = getopt.getopt(sys.argv[1:], 'hlp', ['parsable', 'long', 'help', 'get='])
         except getopt.GetoptError, e:
-            print str(e)
-            print args
+            print "Bad required option"
+            help()
+            exit(3)
+
         for o, a in opts:
             if o in ('l', '--long'):
                 options['outlength'] = 'long'
             elif o in ('p', '--parsable'):
                 options['outlength'] = 'parsable'
+            elif o in ('h', '--help'):
+                help()
+                exit()
+            elif o in ('--get') and a in asset_types:
+                action = 'show'
+                asset_param = a
             else:
-                cmds.append((o, a))
-                
-        for o, a in cmds:
-                
-            if o in ('h', '--help'):
-                print 'helping'
-            elif o in ('--get'):
-                if a in asset_types:
-                    asset_type = a.title()
-                    asset = globals()[asset_type]()
-                    asset.show(options)
-            else:
-                print "unknow option"
+                print "Bad option"
+                help()
                 exit(2)
+
+        asset_type = asset_param.title()
+        asset = globals()[asset_type]()
+        getattr(asset, action)(options)
+
+def help():
+    """
+    Usage:
+
+    %(program)s <action> <asset_type> <output_type>
+
+    <action> - this is what we want to do with asset_type
+
+            --get - gets info about specified asset_type
+
+    <asset_type> - this is asset_type on which we want to perform action
+
+                   currently you can choose from this asset types:
+
+                   cpu, memory, pci, fcms, disk, system
+
+    <output_type> - this specifies format and length of output
+
+                    currently these are available:
+
+                    default is short output, there is no option for that
+
+                    --l or --long - specifies long output
+
+                    --p or --parsable - is long and parsable output
+
+
+    Examples:
+
+            This gets information about system in short format:
+
+                systeminfo --get system
+
+            This gets information about disks in long format:
+
+                systeminfo --get disk --l
+
+                or
+
+                systeminfo --get disk --long
+
+            This gets information about fcms HBA's in parsable format:
+
+                        systeminfo --get fcms --p
+
+        Author:
+
+            Pavol Ipoth
+
+        License:
+
+            GPLv3
+
+"""
+
+    print help.__doc__ % {'program': os.path.split(sys.argv[0])[1]}
 
 main()
